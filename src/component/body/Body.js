10fcb33c549811c98f5e46da813b16a2b4305987
imam hossen <imam.frontend404@gmail.com>
2025-01-08 22:52:03 +0600

    fixingthe show 12 page

--- src/component/body/Body.js
+++ src/component/body/Body.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import Page1 from './Page1';
 import Page2 from './Page2';
-import Page3 from './Page3';
+// import Page3 from './Page3';
 
 
 
@@ -14,7 +14,7 @@ const Body = () => {
         <div>
             <Page1/>
             <Page2/>
-            <Page3/>
+            {/* <Page3/> */}
         </div>
     );
 };
--- src/component/body/Page3.js
+++ src/component/body/Page3.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Page3 = () => {
+const Page3 = ({handleLoadMore}) => {
     return (
         <div>
             <div className='mx-auto my-[40px] lg:my-[50px] xl:my-[70px] 2xl:my-[90px] px-[24px] sm:px-0 lg:px-[50px]'>
                 <div className='flex justify-center items-center'>
-                    <button className='submitbutton bg-[#C6EA60] rounded-[70px] py-[9px] sm:py-[12px] lg:py-[15px] px-[10px] sm:px-[13px] lg:px-[20px] !text-[#000] font-quicksand font-[500]'>Load More Components</button>
+                    <button className='submitbutton bg-[#C6EA60] rounded-[70px] py-[9px] sm:py-[12px] lg:py-[15px] px-[10px] sm:px-[13px] lg:px-[20px] !text-[#000] font-quicksand font-[500]' onClick={handleLoadMore}>Load More Components</button>
                 </div>
             </div>
         </div>
